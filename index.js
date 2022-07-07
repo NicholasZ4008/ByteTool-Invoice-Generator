0d93bccdc68084024ae5272a3a0d38fa356b9115
spw9 <2simwaraich@gmail.com>
2022-07-06 19:52:55 -0700

    adding home

--- index.js
+++ index.js
@@ -64,5 +64,17 @@ express()
             res.end();
         }
     })
+// http://localhost:3000/home
+    .get('/home', function (request, response) {
+        // If the user is loggedin
+        if (request.session.loggedin) {
+            // Output username
+            response.send('Welcome back, ' + request.session.username + '!');
+        } else {
+            // Not logged in
+            response.send('Please login to view this page!');
+        }
+        response.end();
+    })
 
     .listen(PORT, () => console.log(`Listening on ${ PORT }`))
