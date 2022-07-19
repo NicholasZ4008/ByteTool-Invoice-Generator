abb17aba27e041cdde9cd90df9e42938b873c178
spw9 <2simwaraich@gmail.com>
2022-07-19 13:15:47 -0700

    adding auth conn with db

--- index.js
+++ index.js
@@ -122,8 +122,8 @@ express()
         try {
             const dbQuery = await connection.query(`SELECT * FROM accounts WHERE username = '${username}' AND password = '${password}' AND loggedin = 'true';`);
             const results = { 'results': (dbQuery) ? dbQuery.rows : null };
-            console.log(results);
-            if (results[results[0][loggedin]]=='true') {
+            console.log(results.rows.loggedin);
+            if (results.rows.loggedin == 'true') {
                 // Output username
                 response.redirect('/loggedin.html');
             } else {
