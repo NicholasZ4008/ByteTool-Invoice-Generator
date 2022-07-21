d8135dd4b3420a08c6a0b32a74241c12f2a4c93a
spw9 <2simwaraich@gmail.com>
2022-07-20 18:48:09 -0700

    fixing db inserts for username check

--- index.js
+++ index.js
@@ -209,7 +209,7 @@ express()
         }
         console.log(existUsername);
         // Ensure the input fields exists and are not empty
-        if (existUsername.length == 0) {
+        if (existUsername['results'] == null) {
             // Execute SQL query that'll select the account from the database based on the specified username and password
             connection.query(`INSERT INTO accounts (username,password,email,created_on) VALUES ('${username}', '${password}','${email}', CURRENT_TIMESTAMP);`, function (error, results, fields) {
                 // If there is an issue with the query, output the error
