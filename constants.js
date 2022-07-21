043cea9ac26d40e454af41adb564d4d1d9e8a696
spw9 <2simwaraich@gmail.com>
2022-07-20 21:07:59 -0700

    adding constants

---
+++ constants.js
@@ -0,0 +1,21 @@
+//google api integration with nodemailer
+require("dotenv").config();
+
+const auth = {
+    type: "OAuth2",
+    user: "sid.cd.varma@gmail.com",
+    clientId: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    refreshToken: process.env.REFRESH_TOKEN,
+};
+
+const mailoptions = {
+    from: "Siddhant &lt;sid.cd.varma@gmail.com>",
+    to: "sid.cd.varma@gmail.com",
+    subject: "Gmail API NodeJS",
+};
+
+module.exports = {
+    auth,
+    mailoptions,
+};
