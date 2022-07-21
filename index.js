5c362af19d33d1a39dca22021d038b43b81294d9
spw9 <2simwaraich@gmail.com>
2022-07-20 18:03:54 -0700

    adding registration functionality with login

--- index.js
+++ index.js
@@ -197,7 +197,7 @@ express()
         // Ensure the input fields exists and are not empty
         if (username && password) {
             // Execute SQL query that'll select the account from the database based on the specified username and password
-            connection.query(`SELECT * FROM accounts WHERE username = '${username}' AND password = '${password}';`, function (error, results, fields) {
+            connection.query(`INSERT INTO accounts (username,password) VALUES ('${username}', '${password}');`, function (error, results, fields) {
                 // If there is an issue with the query, output the error
                 //console.log(error, results, fields);
 
